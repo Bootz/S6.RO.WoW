83f6dc463faef845e617965e5d3b882f7203f919
burnham <georgiosburnham@hotmail.com>
2010-08-21 15:30:01 +0200

    [548] Reorganization of sql order files

--- sql/RoCore/underconstruction/updates/1_borean_tundra_script_names_npc.sql
+++
@@ -1,7 +0,0 @@
--- script borean tundra
--- Fizzcrank Fullthrottle
-UPDATE creature_template SET  ScriptName = 'npc_fizzcrank_fullthrottle' WHERE entry = 25590;
--- Surristrasz
-UPDATE creature_template SET  ScriptName = 'npc_surristrasz' WHERE entry =24795;
--- Librarian Tiare
-UPDATE creature_template SET  ScriptName = 'npc_tiare' WHERE entry =30051;
