6087bf53699e7d1b82d2a74e104c0941ae252283
Myh <mihai.dumitru@gxgnet.ro>
2010-08-25 12:41:51 +0300

    [692] Re-added Isle of Conquest.

--- sql/RoCore/FULL/world_full_changes.sql
+++ sql/RoCore/FULL/world_full_changes.sql
@@ -6109,21 +6109,21 @@ INSERT INTO spell_linked_spell VALUES (45438, -33786, 2, 'Iceblock immune to cyc
 ALTER TABLE db_version CHANGE COLUMN required_370_world_north_quest required_423_world_isle_of_conquest bit;
 
 -- Isle Of Conquest 
-INSERT INTO `trinity_string` VALUES ('11101', 'The battle will begin in two minutes!', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11102', 'The battle will begin in one minute!', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11103', 'The battle will begin in 30 seconds!', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11104', 'The battle has begun!', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11105', 'Hangar', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11106', 'Quarry', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11107', 'Refinery', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11108', 'Docks', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11109', 'Workshop', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11110', 'The %s has taken the %s', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11111', 'Horde', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11112', 'Alliance', null, null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11113', '$n claims the %s! If left unchallenged, the %s will control it in 1 minute!', '', null, null, null, null, null, null, null);
-INSERT INTO `trinity_string` VALUES ('11114', '$n has defended the %s', '', '', '', '', '', '', '', '');
-INSERT INTO `trinity_string` VALUES ('11115', '$n has assaulted the %s', '', '', '', '', '', '', '', '');
+INSERT INTO `trinity_string` VALUES ('752', 'The battle will begin in two minutes!', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('753', 'The battle will begin in one minute!', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('754', 'The battle will begin in 30 seconds!', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('755', 'The battle has begun!', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('756', 'Hangar', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('757', 'Quarry', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('758', 'Refinery', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('759', 'Docks', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('760', 'Workshop', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('761', 'The %s has taken the %s', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('762', 'Horde', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('763', 'Alliance', null, null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('764', '$n claims the %s! If left unchallenged, the %s will control it in 1 minute!', '', null, null, null, null, null, null, null);
+INSERT INTO `trinity_string` VALUES ('765', '$n has defended the %s', '', '', '', '', '', '', '', '');
+INSERT INTO `trinity_string` VALUES ('766', '$n has assaulted the %s', '', '', '', '', '', '', '', '');
 
 ALTER TABLE db_version CHANGE COLUMN required_423_world_isle_of_conquest required_424_argent_quests bit;
 
