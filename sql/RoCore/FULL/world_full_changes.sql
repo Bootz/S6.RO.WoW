10f908663bfd5adf3a5df1e3458ab9d5322f106a
burnham <georgiosburnham@hotmail.com>
2010-09-20 14:45:34 +0200

    [934] Mage: T7 P2 Bonus Mage.

--- sql/RoCore/FULL/world_full_changes.sql
+++ sql/RoCore/FULL/world_full_changes.sql
@@ -6208,6 +6208,13 @@ UPDATE `creature_template` SET `faction_A` = 14, `faction_H` = 14, `unit_flags` 
 -- Kel Thuzad immunity http://www.wowhead.com/npc=15990
 UPDATE `creature_template` SET `mechanic_immune_mask` = 617299839 WHERE `entry` IN (15990, 30061);
 
+-- Mage: T7 P2 Bonus Mage
+-- 2135 Improved Mana Gems 
+DELETE FROM `spell_proc_event` WHERE `entry` IN (37447, 61062);
+INSERT INTO `spell_proc_event` (`entry`,`SchoolMask`,`SpellFamilyName`,`SpellFamilyMask0`,`SpellFamilyMask1`,`SpellFamilyMask2`,`procFlags`,`procEx`,`ppmRate`,`CustomChance`,`Cooldown`) VALUES 
+(37447, 0, 3, 0, 0x00000100, 0, 0x04000, 0, 0, 0, 0), -- Serpent-Coil Braid
+(61062, 0, 3, 0, 0x00000100, 0, 0x04000, 0, 0, 0, 0); -- 2/5 Frostfire Garb
+
 -- ----------------- --
 -- Utgarde Pinnacle. --
 -- ----------------- --
