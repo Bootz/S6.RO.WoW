5187e7d43373f76e38407e97ce67cc8c53deb5be
burnham <georgiosburnham@hotmail.com>
2010-09-20 17:11:08 +0200

    [945] Fix Hunter: Hunting Party.

--- sql/RoCore/FULL/world_full_changes.sql
+++ sql/RoCore/FULL/world_full_changes.sql
@@ -6226,6 +6226,12 @@ REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 REPLACE INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
 ('-8129','spell_pri_mana_burn');
 
+-- rc 945 Hunter: Hunting Party
+REPLACE INTO spell_proc_event (entry, SchoolMask, SpellFamilyName, SpellFamilyMask0, SpellFamilyMask1, SpellFamilyMask2, procFlags, procEx, ppmRate, CustomChance, Cooldown) VALUES
+(53290,0,9,0x800,0x1,0x200,0,0x0000002,0,0,0),
+(53291,0,9,0x800,0x1,0x200,0,0x0000002,0,0,0),
+(53292,0,9,0x800,0x1,0x200,0,0x0000002,0,0,0);
+
 -- ----------------- --
 -- Utgarde Pinnacle. --
 -- ----------------- --
